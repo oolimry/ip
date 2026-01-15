2127aec05685eae9427c8254ef2faf38e9e9b178
oolimry <limruiy@gmail.com>
2026-01-15 22:14:30 +0800

    Changed the name to Ducky and added placeholder text

---
+++ src/main/java/Ducky.java
@@ -0,0 +1,6 @@
+public class Ducky {
+    public static void main(String[] args) {
+        System.out.println("Hi! My name is Ducky!");
+        System.out.println("I'm useless for now, so goodbye!");
+    }
+}
