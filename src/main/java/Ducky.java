9bedf74dddb1526ba31e03deea89325cb44c4cd6
oolimry <limruiy@gmail.com>
2026-01-16 19:50:12 +0800

    added input functionality and now just echoes the input

--- src/main/java/Ducky.java
+++ src/main/java/Ducky.java
@@ -1,6 +1,21 @@
+import java.util.Scanner; 
+
 public class Ducky {
     public static void main(String[] args) {
         System.out.println("Hi! My name is Ducky!");
-        System.out.println("I'm useless for now, so goodbye!");
+        System.out.println("I can repeat whatever you said :)");
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+
+            else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
