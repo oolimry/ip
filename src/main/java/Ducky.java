e8cae2a27072c1c546777eff899ec0588d60a55c
oolimry <limruiy@gmail.com>
2026-01-19 12:56:50 +0800

    add 3 types of task: todo, deadline, event. added a string parser for more complex input parsing in the future

--- src/main/java/Ducky.java
+++ src/main/java/Ducky.java
@@ -1,5 +1,6 @@
 import java.util.Scanner; 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Ducky {
     public static void main(String[] args) {
@@ -12,11 +13,10 @@ public class Ducky {
             Scanner scanner = new Scanner(System.in);
             String userInput = scanner.nextLine();
 
-            String[] userInputArgs = userInput.split("\\s+");
+            HashMap<String, String> parsedInput = parseInputs(userInput);
 
-            //System.out.println(userInputArgs);
-
-            String mainCommand = userInputArgs[0];
+            String mainCommand = parsedInput.get("command");
+            String commandValue = parsedInput.get("commandValue");
 
             if (userInput.equals("bye")) {
                 break;
@@ -31,7 +31,7 @@ public class Ducky {
             }
 
             else if (mainCommand.equals("mark")) {
-                int taskNumber = Integer.parseInt(userInputArgs[1]);
+                int taskNumber = Integer.parseInt(commandValue);
 
                 tasks.get(taskNumber - 1).markAsDone();
 
@@ -40,7 +40,7 @@ public class Ducky {
             }
 
             else if (mainCommand.equals("unmark")) {
-                int taskNumber = Integer.parseInt(userInputArgs[1]);
+                int taskNumber = Integer.parseInt(commandValue);
 
                 tasks.get(taskNumber - 1).ummarkAsDone();
 
@@ -48,11 +48,65 @@ public class Ducky {
                     + tasks.get(taskNumber - 1).getDescription() + "\" as not done");
             }
 
-            else {
-                tasks.add(new Task(userInput));
+            else if (mainCommand.equals("todo")) {
+                ToDoTask newTask = new ToDoTask(commandValue);
+                tasks.add(newTask);
 
-                System.out.println("Added task: " + userInput);
+                System.out.println("Added todo: " + newTask);
+            }
+
+            else if (mainCommand.equals("deadline")) {
+                String deadline = parsedInput.get("by");
+                DeadlineTask newTask = new DeadlineTask(commandValue, deadline);
+                tasks.add(newTask);
+
+                System.out.println("Added deadline: " + newTask);
+            }
+
+            else if (mainCommand.equals("event")) {
+                String from = parsedInput.get("from");
+                String to = parsedInput.get("to");
+                EventTask newTask = new EventTask(commandValue, from, to);
+                tasks.add(newTask);
+
+                System.out.println("Added event: " + newTask);
+            }
+
+
+            else {
+                System.out.println("Command not found!");
             }
         }
     }
+
+    // returns an ArrayList containing the format of the pair (param name, thing)
+    // ArrayList[0].first always gives you the main command name
+    public static HashMap<String, String> parseInputs(String input) {
+        HashMap<String, String> result = new HashMap<String, String>();
+
+        String[] segments = input.split("/");
+
+        for(String segment : segments){
+            String[] tokens = segment.split("\\s+");
+            String param = tokens[0];
+            String value = "";
+            for (int i = 1; i < tokens.length; i++) {
+                if (i != 1)
+                    value += " "; 
+                value += tokens[i];
+            }
+
+            if (segment == segments[0]) {
+                result.put("command", param);
+                result.put("commandValue", value);
+            }
+            
+            else {
+                result.put(param, value);
+            }
+        }
+
+
+        return result;
+    } 
 }
