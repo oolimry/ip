5d35882d68bc5b5d99a96d66592ac622195ce856
oolimry <limruiy@gmail.com>
2026-01-24 18:40:56 +0800

    create package ducky

--- src/main/java/Constants.java
+++
@@ -1,4 +0,0 @@
-public class Constants {
-    static final String OUTPUT_DATE_FORMAT = "MMM d yyyy";
-    static final String SAVE_DATE_FORMAT = "yyyy-MM-dd";
-}
--- src/main/java/TaskList.java
+++
@@ -1,5 +0,0 @@
-import java.util.ArrayList;
-
-class TaskList extends ArrayList<Task> {
-    
-}
