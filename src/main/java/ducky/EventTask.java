2611ade700eb77d9d76234af1739869483d30178
oolimry <limruiy@gmail.com>
2026-01-27 18:42:29 +0800

    add javadoc to functions

--- src/main/java/ducky/EventTask.java
+++ src/main/java/ducky/EventTask.java
@@ -8,12 +8,20 @@ public class EventTask extends Task {
     private LocalDate from;
     private LocalDate to;
 
+    /**
+     * @param description Description of Event
+     * @param from Starting date of event
+     * @param to Ending date of event
+     */
     public EventTask(String description, LocalDate from, LocalDate to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * @return the event printed in format for user to see
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() +
@@ -21,6 +29,9 @@ public class EventTask extends Task {
                 " to: " + to.format(DateTimeFormatter.ofPattern(Constants.OUTPUT_DATE_FORMAT)) + ")";
     }
 
+    /**
+     * @return the format for saving, which is similar to the reading format
+     */
     @Override
     public String toSaveFormat() {
         String res = "event " + description +
--- src/main/java/ducky/ToDoTask.java
+++ src/main/java/ducky/ToDoTask.java
@@ -2,15 +2,24 @@ package ducky;
 
 public class ToDoTask extends Task {
 
+    /**
+     * @param description the description of the todo
+     */
     public ToDoTask(String description) {
         super(description);
     }
 
+    /**
+     * @return the to_do printed in format for user to see
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
 
+    /**
+     * @return the format for saving, which is similar to the reading format
+     */
     @Override
     public String toSaveFormat() {
         String res = "todo " + description;
